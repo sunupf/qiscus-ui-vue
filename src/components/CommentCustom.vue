78a38f1ed8f966017ef49fe8283db4978f1ed288
desertlion <fikri.desertlion@gmail.com>
2018-02-01 16:01:58 +0700

    Add custom template and carousel

---
+++ src/components/CommentCustom.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="qcw-custom-template" v-html="finalTemplate">
+  </div>
+</template>
+
+<script>
+import QiscusCore from '../lib/SDKCore';
+
+export default {
+  name: 'CommentCustom',
+  props: ['data'],
+  data() {
+    return {
+      finalTemplate: '',
+    };
+  },
+  mounted() {
+    // ambil dulu templatenya
+    let temp = QiscusCore.templateFunction(this.data);
+    if (!temp) { this.finalTemplate = '<div>No template provided</div>'; return false; }
+    const rgx = /{(.*?)}/g;
+    const matched = temp.match(rgx) || [];
+    matched.forEach((r) => {
+      // ambil array datanya
+      let val = this.data;
+      r.substring(1, r.length - 1)
+        .split('.')
+        .forEach((k) => {
+          val = val[k];
+        });
+      temp = temp.replace(r, val);
+    });
+    this.finalTemplate = temp;
+    return true;
+  },
+};
+</script>
