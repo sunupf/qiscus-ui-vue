128a76ea27d321113246ab3f8a58f1a06c6af4de
Sunu Pinasthika Fajar <sunupf@gmail.com>
2018-02-12 15:49:14 +0700

    remove autofocus js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,3 @@
-export function focusMessageForm() {
-  const element = document.querySelector('.qcw-comment-form textarea');
-  // element.value += e.native;
-  if(element) element.focus();
-}
-
 export function scrollIntoElement(core) {
   const latestCommentId = core
     .selected.comments[core.selected.comments.length - 1].id;
