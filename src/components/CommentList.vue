606b8e372caeecc8834fb5ee95cf3ea0e9af6a78
apiep <apiep.oedin@gmail.com>
2018-04-18 15:09:38 +0700

    Fix scroll jumping when new message arrived

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-  div.qcw-comments
+  div.qcw-comments(@scroll="onScroll($event)"
+    ref="scrollTarget"
+    :class="{'isReading': isReading}")
     div.inner
       div.qcw-load-comment-indicator(v-if="core.isLoading")
         loader()
@@ -29,6 +31,7 @@
             :onupdate="onupdate"
             :userData="core.userData"
             :showAvatar="core.options.avatar"
+            ref="comment"
           )
         //- component for uploader progress
 
@@ -42,20 +45,22 @@ import FileDragDrop from './FileDragDrop';
 import { scrollIntoLastElement } from '../lib/utils';
 
 export default {
-  name: 'Comments',
+  name: 'CommentList',
   components: { Icon, Loader, Comment, FileDragDrop },
   props: ['core', 'onClickImage', 'onupdate', 'replyHandler'],
+  data() {
+    return {
+      isLoadingMore: false,
+      isReading: false,
+      commentLength: 0,
+      dragging: false,
+      timeoutId: -1,
+    };
+  },
   computed: {
     comments() {
       return this.core.selected.comments;
     },
-  },
-  data() {
-    return {
-      isLoadingMore: false,
-      commentLength: 0,
-      dragging: false,
-    };
   },
   updated() {
     if (this.core.selected) {
@@ -64,29 +69,26 @@ export default {
         const lastComment = this.comments[lastCommentIndex];
         this.core.readComment(this.core.selected.id, lastComment.id);
         this.commentLength = this.comments.length;
-        if (!this.core.UI.isReading) scrollIntoLastElement(this.core);
+        if (!this.isReading) scrollIntoLastElement(this.core);
       }
     }
   },
-  mounted() {
-    const self = this;
-    // attach scroll listener
-    const scrollContainer = document.querySelector('.qcw-comments');
-    if (scrollContainer != null) {
-      scrollContainer.onscroll = () => {
-        const scrollHeight =  scrollContainer.scrollHeight;
-        const clientHeight = scrollContainer.clientHeight;
-        const scrollTop = scrollContainer.scrollTop;
-        const scrollTreshold = 2.3 * clientHeight;
-        if (scrollHeight - scrollTop > scrollTreshold) {
-          self.core.UI.isReading = true;
-        } else {
-          self.core.UI.isReading = false;
-        }
-      };
-    }
-  },
   methods: {
+    onScroll(event) {
+      if (this.timeoutId !== -1) {
+        window.clearTimeout(this.timeoutId);
+      }
+      this.timeoutId = window.setTimeout(() => {
+        const $element = event.target;
+        const scrollHeight = $element.scrollHeight;
+        const clientHeight = $element.clientHeight;
+        const scrollTop = $element.scrollTop;
+        const scrollTreshold = 1.2 * clientHeight;
+        const calculation = scrollHeight - scrollTop;
+        this.isReading = calculation > scrollTreshold;
+        this.timeoutId = -1;
+      }, 300);
+    },
     loadMore() {
       this.isLoadingMore = true;
       this.core.loadMore(this.comments[0].id).then(() => {
