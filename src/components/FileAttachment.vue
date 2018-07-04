76d251bca928fd828fe0a06d53e93f40811229a5
Rijalul Fikri <fikri.desertlion@gmail.com>
2018-07-04 07:01:36 +0700

    Add support for video and audio rendering

--- src/components/FileAttachment.vue
+++ src/components/FileAttachment.vue
@@ -1,10 +1,23 @@
 <template lang="pug">
   div
+    audio(
+      v-if="fileType == 'audio'"
+      :src="comment.payload.url"
+      width="100%"
+      controls) Your browser does not support the audio element.
+    video(
+      controls
+      v-if="fileType == 'video'"
+      muted
+      :src="comment.payload.url"
+      width="100%") Sorry, your browser doesn't support embedded videos.
     image-loader(
+      v-else
       :comment="comment"
       :message="comment.message"
       :on-click-image="onClickImage"
       :callback="callback")
+
     comment-render(:text="comment.payload.caption" v-if="comment.payload.caption != ''")
 </template>
 
@@ -16,5 +29,26 @@ export default {
   name: 'FileAttachment',
   components: { ImageLoader, CommentRender },
   props: ['comment', 'onClickImage', 'callback'],
+  data() {
+    return {
+      fileType: null,
+    };
+  },
+  mounted() {
+    this.fileType = this.checkfileType(this.comment.payload);
+  },
+  methods: {
+    checkfileType(payload) {
+      const splittedURL = payload.url.split('.');
+      const ext = splittedURL[splittedURL.length - 1].toLowerCase();
+      const imageExts = ['jpg', 'jpeg', 'gif', 'webp'];
+      const videoExts = ['mov', 'mp4', 'webm'];
+      const audioExts = ['mp3'];
+      if (imageExts.includes(ext)) return 'image';
+      if (videoExts.includes(ext)) return 'video';
+      if (audioExts.includes(ext)) return 'audio';
+      return 'unavailable';
+    },
+  },
 };
 </script>
